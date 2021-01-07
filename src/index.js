cb72edc4e91f87e7def093d565e0488f4781433e
Michael A. Vickers <mavickers@lightpathllc.com>
2021-01-07 14:04:14 -0500

    Migration of the rest of the classes.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
+import Classes from "./base/Classes";
 import Describing from "./base/interfaces/Describing";
 import Enum from "./base/interfaces/Enum";
 import Flags from "./base/interfaces/Flags";
+import Mappable from "./base/interfaces/Mappable";
 import Mapper from "./base/classes/Mapper";
 import MapperOptionsFlag from "./base/classes/MapperOptionsFlag";
+import StandardModel from "./base/interfaces/StandardModel";
 import Utilities from "./base/Utilities";
 
 export {
+    Classes,
     Describing,
     Enum,
     Flags,
+    Mappable,
     Mapper,
     MapperOptionsFlag,
+    StandardModel,
     Utilities
 };
 
 export default {
+    Classes,
     Describing: Describing,
     Enum: Enum,
     Flags: Flags,
+    Mappable: Mappable,
     Mapper: Mapper,
     MapperOptionsFlag: MapperOptionsFlag,
+    StandardModel: StandardModel,
     Utilities: Utilities
 };
