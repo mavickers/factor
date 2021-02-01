a454baec7d01e67d7f6ff56f8ccc403b3b496865
Michael A. Vickers <mavickers@lightpathllc.com>
2021-02-01 15:01:22 -0500

    Cleanup.

--- src/base/interfaces/Describable.js
+++ src/base/interfaces/Describable.js
@@ -3,34 +3,6 @@ import Utilities from "../Utilities";
 const configId = Symbol("describable");
 
 class Describable {
-    differencesFrom(obj) {
-        const diffs = { };
-
-        Object.getOwnPropertyNames(this)
-            .filter(prop =>
-                prop != "constructor" &&
-                prop != "__ob__" &&
-                typeof this[prop] != "function" &&
-                !(this[prop] instanceof Function)
-            )
-            .forEach(prop => {
-                const hashFn = Utilities.isFunction(this.constructor[configId]?.["hashFn"] ?? false) && this.constructor[configId]?.["hashFn"] || throw Error("Hash function not available");
-
-                // if the property is an object and the hash of the property of this object is same
-                // to the hash of the property of the comparing object, the property not considered different.
-                if (this[prop] instanceof Object && obj[prop] && hashFn(this[prop]) == hashFn(obj[prop])) return;
-
-                // if this property value is not an object and the property of this object has the same
-                // value as the property value of the comparing object the project is not considered different.
-                if (!(this[prop] instanceof Object) && this[prop] == obj[prop]) return;
-
-                // it seems the property value is different, so add it to the list of different values.
-                diffs[prop] = obj[prop];
-            });
-
-        return diffs;
-    }
-
     get className() {
         return this.__proto__.constructor.name;
     }
