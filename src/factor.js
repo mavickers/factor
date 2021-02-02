27532c064a8e9b730eb3006e651c2e6f31525853
Michael A. Vickers <mavickers@lightpathllc.com>
2021-02-02 17:56:38 -0500

    Cleanup.

--- src/factor.js
+++ src/factor.js
@@ -1,60 +1,60 @@
-// import Globals from "./base/Globals";
-//
-// import Classes from "./base/Classes";
+import Globals from "./base/Globals";
+
+import Classes from "./base/Classes";
 import Configurable from "./base/interfaces/Configurable";
-// import Describable from "./base/interfaces/Describable";
-// import Mappable from "./base/interfaces/Mappable";
-// import Mixin from "./base/classes/Mixin";
-// import StandardModel from "./base/classes/StandardModel";
-//
-// import Logger from "./base/components/Logger/Logger";
-//
-// import Pipeline from "./base/components/Pipeline/Pipeline";
-// import PipelineArgs from "./base/components/Pipeline/PipelineArgs";
-// import PipelineFilter from "./base/components/Pipeline/PipelineFilter";
-//
-// import Enum from "./base/classes/Enum";
-// import Flags from "./base/classes/Flags";
-// import Mapper from "./base/classes/Mapper";
-// import MapperOptionsFlag from "./base/classes/MapperOptionsFlag";
-//
+import Describable from "./base/interfaces/Describable";
+import Mappable from "./base/interfaces/Mappable";
+import Mixin from "./base/classes/Mixin";
+import StandardModel from "./base/classes/StandardModel";
+
+import Logger from "./base/components/Logger/Logger";
+
+import Pipeline from "./base/components/Pipeline/Pipeline";
+import PipelineArgs from "./base/components/Pipeline/PipelineArgs";
+import PipelineFilter from "./base/components/Pipeline/PipelineFilter";
+
+import Enum from "./base/classes/Enum";
+import Flags from "./base/classes/Flags";
+import Mapper from "./base/classes/Mapper";
+import MapperOptionsFlag from "./base/classes/MapperOptionsFlag";
+
 import Utilities from "./base/Utilities";
 
 export {
-    // Classes,
+    Classes,
     Configurable,
-    // Describable,
-    // Enum,
-    // Flags,
-    // Globals,
-    // Logger,
-    // Mappable,
-    // Mapper,
-    // MapperOptionsFlag,
-    // Mixin,
-    // Pipeline,
-    // PipelineArgs,
-    // PipelineFilter,
-    // StandardModel,
+    Describable,
+    Enum,
+    Flags,
+    Globals,
+    Logger,
+    Mappable,
+    Mapper,
+    MapperOptionsFlag,
+    Mixin,
+    Pipeline,
+    PipelineArgs,
+    PipelineFilter,
+    StandardModel,
     Utilities
 };
 
 export default {
-    // AutoModel: StandardModel,
-    // Classes,
+    AutoModel: StandardModel,
+    Classes,
     Configurable,
-    // Describing: Describable,
-    // Enum: Enum,
-    // Flags: Flags,
-    // Globals: Globals,
-    // Logger: Logger,
-    // Mappable: Mappable,
-    // Mapper: Mapper,
-    // MapperOptionsFlag: MapperOptionsFlag,
-    // Mixin: Mixin,
-    // Pipeline,
-    // PipelineArgs,
-    // PipelineFilter,
-    // StandardModel: StandardModel,
+    Describing: Describable,
+    Enum: Enum,
+    Flags: Flags,
+    Globals: Globals,
+    Logger: Logger,
+    Mappable: Mappable,
+    Mapper: Mapper,
+    MapperOptionsFlag: MapperOptionsFlag,
+    Mixin: Mixin,
+    Pipeline,
+    PipelineArgs,
+    PipelineFilter,
+    StandardModel: StandardModel,
     Utilities: Utilities
 };
