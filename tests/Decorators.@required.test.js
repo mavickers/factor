cc8fbb0cc948b2abbb0cf34e303693cabb9401d7
Michael A. Vickers <mavickers@lightpathllc.com>
2021-02-08 17:46:04 -0500

    Cleanup @required tests.

--- tests/Decorators.@required.test.js
+++ tests/Decorators.@required.test.js
@@ -2,13 +2,11 @@ import "jest-extended";
 import { required } from "../src/base/classes/decorators";
 
 describe("@required decorator tests", () => {
-    it("should apply decorator properly", () => {
+    it("should operate properly on fields when instantiating class", () => {
         let Class1;
         let Class2;
         let model1;
 
-        // if @required is invoked on a field that does not set a value at
-        // class definition it should throw.
         expect(() => Class1 = class { @required field1; }).toThrow();
         expect(Class1).toBeUndefined();
 
@@ -17,9 +15,14 @@ describe("@required decorator tests", () => {
         expect(model1.field1).toBeFalse();
         expect(() => model1.field1 = true).not.toThrow();
         expect(model1.field1).toBeTrue();
+    });
 
-        // @required should cause a field assignment to null or
-        // undefined to throw
+    it("should throw on null or undefined field assignment" , () => {
+        let Class1, model1;
+
+        expect(() => Class1 = class { @required field1 = true; }).not.toThrow();
+        expect(() => model1 = new Class1()).not.toThrow();
+
         expect(() => model1.field1 = null).toThrow();
         expect(model1.field1).toBeTrue();
         expect(() => model1.field1 = undefined).toThrow();
@@ -39,5 +42,6 @@ describe("@required decorator tests", () => {
         expect(() => model1 = new Class1()).not.toThrow();
         expect(model1.field1).toBeTrue();
         expect(model1.field2).toBeUndefined();
+        expect(model1.field3).toBeFalse();
     });
 })
