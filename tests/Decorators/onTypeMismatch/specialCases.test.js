73fff6fdb2a988171a7d9c3fb925bdd6c5d42abe
Michael A. Vickers <mavickers@lightpathllc.com>
2021-02-22 16:57:06 -0500

    Refactor special cases tests file for onTypeMismatch.

---
+++ tests/Decorators/onTypeMismatch/specialCases.test.js
@@ -0,0 +1,18 @@
+import "jest-extended";
+import { is } from "../../../src/base/classes/decorators";
+import TypeMismatchSetOptions from "../../../src/base/classes/flags/TypeMismatchSetOptions";
+import onTypeMismatch, { mismatchConfig } from "../../../src/base/classes/decorators/onTypeMismatch";
+
+let Class1, Class2, Class3,
+    model1, model2, model3;
+
+describe("@onTypeMismatch decorator special cases tests", () => {
+    beforeEach(() => {
+       Class1 = Class2 = Class3 = undefined;
+       model1 = model2 = model3 = undefined;
+    });
+
+    it.skip("should handle special cases properly", () => {
+        // todo: multiple on the same field/class
+    });
+})
