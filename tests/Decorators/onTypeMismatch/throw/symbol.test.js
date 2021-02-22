618b89326f563215f857a5df0969bfeeea8accfd
Michael A. Vickers <mavickers@lightpathllc.com>
2021-02-22 12:33:07 -0500

    Add onTypeMismatch tests for throw.symbol.

--- tests/Decorators/onTypeMismatch/throw/symbol.test.js
+++ tests/Decorators/onTypeMismatch/throw/symbol.test.js
@@ -4,10 +4,12 @@ import TypeMismatchSetOptions from "../../../../src/base/classes/flags/TypeMisma
 import onTypeMismatch, { mismatchConfig } from "../../../../src/base/classes/decorators/onTypeMismatch";
 
 let Class1, Class2, Class3,
-    model1, model2, model3;
+    model1, model2, model3,
+    symbol1, symbol2, symbol3;
 
-describe("@onTypeMismatch decorator tests throw option for boolean type", () => {
+describe("@onTypeMismatch decorator tests throw option for symbol type", () => {
     beforeEach(() => {
+
        Class1 = Class2 = Class3 = undefined;
        model1 = model2 = model3 = undefined;
     });
@@ -15,18 +17,23 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
     describe("when set on classes", () => {
         beforeEach(() => {
 
+            symbol1 = Symbol(1);
+            symbol2 = Symbol(2);
+            symbol3 = Symbol(3);
+
+
             Class1 = @onTypeMismatch("Throw") class {
-                @is(String) field1;
-                @is("string") field2;
-                @is(String) field3 = "true";
-                @is("string") field4 = "false";
+                @is(Symbol) field1;
+                @is("symbol") field2;
+                @is(Symbol) field3 = symbol1;
+                @is("symbol") field4 = symbol2;
             }
 
             Class2 = @onTypeMismatch(TypeMismatchSetOptions.Throw) class {
-                @is(String) field1;
-                @is("string") field2;
-                @is(String) field3 = "true";
-                @is("string") field4 = "false";
+                @is(Symbol) field1;
+                @is("symbol") field2;
+                @is(Symbol) field3 = symbol1;
+                @is("symbol") field4 = symbol2;
             }
 
             expect(() => model1 = new Class1()).not.toThrow();
@@ -34,8 +41,8 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
         });
 
        it("should throw when class is defined with type mismatches", () => {
-            expect(() => @onTypeMismatch("Throw") class { @is(String) field1 = true }).toThrow();
-            expect(() => @onTypeMismatch(TypeMismatchSetOptions.Throw) class { @is(String) field1 = false }).toThrow();
+            expect(() => @onTypeMismatch("Throw") class { @is(Symbol) field1 = true }).toThrow();
+            expect(() => @onTypeMismatch(TypeMismatchSetOptions.Throw) class { @is(Symbol) field1 = false }).toThrow();
        });
 
        it("should have initial values set properly", () => {
@@ -43,8 +50,8 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
 
            expect(model1.field1).toBeUndefined();
            expect(model1.field2).toBeUndefined();
-           expect(model1.field3).toEqual("true");
-           expect(model1.field4).toEqual("false");
+           expect(model1.field3).toEqual(symbol1);
+           expect(model1.field4).toEqual(symbol2);
        });
 
        it("should throw when fields are not set after instantiation according to type", () => {
@@ -59,34 +66,34 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
        });
 
        it("should allow fields to set to their proper values", () => {
-           expect(() => model1.field1 = "true").not.toThrow();
-           expect(model1.field1).toEqual("true");
-           expect(() => model1.field2 = "false").not.toThrow();
-           expect(model1.field2).toEqual("false");
-           expect(() => model2.field1 = "true").not.toThrow();
-           expect(model1.field1).toEqual("true");
-           expect(() => model2.field2 = "false").not.toThrow();
-           expect(model1.field2).toEqual("false");
+           expect(() => model1.field1 = symbol1).not.toThrow();
+           expect(model1.field1).toEqual(symbol1);
+           expect(() => model1.field2 = symbol2).not.toThrow();
+           expect(model1.field2).toEqual(symbol2);
+           expect(() => model2.field1 = symbol1).not.toThrow();
+           expect(model1.field1).toEqual(symbol1);
+           expect(() => model2.field2 = symbol2).not.toThrow();
+           expect(model1.field2).toEqual(symbol2);
        });
     });
 
     describe("when set on fields", () => {
         beforeEach(() => {
             Class1 = class {
-                @is(String) @onTypeMismatch("Throw") field1;
-                @is("string") @onTypeMismatch("Throw") field2;
-                @is(String) field3;
-                @is("string") field4;
-                @is(String) @onTypeMismatch(TypeMismatchSetOptions.Throw) field5;
-                @is("string") @onTypeMismatch(TypeMismatchSetOptions.Throw) field6;
-                @is(String) @onTypeMismatch("Throw") field7 = "true";
-                @is("string") @onTypeMismatch("Throw") field8 = "false";
-                @is(String) @onTypeMismatch(TypeMismatchSetOptions.Throw) field9 = "true";
-                @is("string") @onTypeMismatch(TypeMismatchSetOptions.Throw) field10 = "false";
+                @is(Symbol) @onTypeMismatch("Throw") field1;
+                @is("symbol") @onTypeMismatch("Throw") field2;
+                @is(Symbol) field3;
+                @is("symbol") field4;
+                @is(Symbol) @onTypeMismatch(TypeMismatchSetOptions.Throw) field5;
+                @is("symbol") @onTypeMismatch(TypeMismatchSetOptions.Throw) field6;
+                @is(Symbol) @onTypeMismatch("Throw") field7 = symbol1;
+                @is("symbol") @onTypeMismatch("Throw") field8 = symbol2;
+                @is(Symbol) @onTypeMismatch(TypeMismatchSetOptions.Throw) field9 = symbol1;
+                @is("symbol") @onTypeMismatch(TypeMismatchSetOptions.Throw) field10 = symbol2;
             }
 
             Class2 = @onTypeMismatch("Throw") class {
-                @is(String) field1;
+                @is(Symbol) field1;
                 field2;
             }
 
@@ -100,21 +107,21 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
             expect(model1.field4).toBeUndefined();
             expect(model1.field5).toBeUndefined();
             expect(model1.field6).toBeUndefined();
-            expect(model1.field7).toEqual("true");
-            expect(model1.field8).toEqual("false");
-            expect(model1.field9).toEqual("true");
-            expect(model1.field10).toEqual("false");
+            expect(model1.field7).toEqual(symbol1);
+            expect(model1.field8).toEqual(symbol2);
+            expect(model1.field9).toEqual(symbol1);
+            expect(model1.field10).toEqual(symbol2);
         });
 
         it("should throw when fields are not set at instantiation according to type", () => {
             let Class4, model4;
-            expect(() => Class4 = class { @is(String) @onTypeMismatch("Throw") field1 = true }).toThrow();
+            expect(() => Class4 = class { @is(Symbol) @onTypeMismatch("Throw") field1 = true }).toThrow();
             expect(Class4).toBeUndefined();
-            expect(() => Class4 = class { @is("string") @onTypeMismatch("Throw") field1 = true }).toThrow();
+            expect(() => Class4 = class { @is("symbol") @onTypeMismatch("Throw") field1 = true }).toThrow();
             expect(Class4).toBeUndefined();
-            expect(() => Class4 = class { @is(String) @onTypeMismatch(TypeMismatchSetOptions.Throw) field1 = true }).toThrow();
+            expect(() => Class4 = class { @is(Symbol) @onTypeMismatch(TypeMismatchSetOptions.Throw) field1 = true }).toThrow();
             expect(Class4).toBeUndefined();
-            expect(() => Class4 = class { @is("string") @onTypeMismatch(TypeMismatchSetOptions.Throw) field1 = true }).toThrow();
+            expect(() => Class4 = class { @is("symbol") @onTypeMismatch(TypeMismatchSetOptions.Throw) field1 = true }).toThrow();
             expect(Class4).toBeUndefined();
         });
 
@@ -136,36 +143,36 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
         });
 
         it("should allow fields to set to their proper values", () => {
-            expect(() => model1.field1 = "true").not.toThrow();
-            expect(model1.field1).toEqual("true");
-            expect(() => model1.field2 = "false").not.toThrow();
-            expect(model1.field2).toEqual("false");
-            expect(() => model1.field5 = "true").not.toThrow();
-            expect(model1.field5).toEqual("true");
-            expect(() => model1.field6 = "false").not.toThrow();
-            expect(model1.field6).toEqual("false");
+            expect(() => model1.field1 = symbol1).not.toThrow();
+            expect(model1.field1).toEqual(symbol1);
+            expect(() => model1.field2 = symbol2).not.toThrow();
+            expect(model1.field2).toEqual(symbol2);
+            expect(() => model1.field5 = symbol1).not.toThrow();
+            expect(model1.field5).toEqual(symbol1);
+            expect(() => model1.field6 = symbol2).not.toThrow();
+            expect(model1.field6).toEqual(symbol2);
 
             // this tests that default type mismatch does not interfere
             // when values are set properly
-            expect(() => model1.field3 = "true").not.toThrow();
-            expect(() => model1.field4 = "false").not.toThrow();
-            expect(model1.field3).toEqual("true");
-            expect(model1.field4).toEqual("false");
+            expect(() => model1.field3 = symbol1).not.toThrow();
+            expect(() => model1.field4 = symbol2).not.toThrow();
+            expect(model1.field3).toEqual(symbol1);
+            expect(model1.field4).toEqual(symbol2);
         });
 
         it("should override class option when set on field", () => {
             expect(() => {
                 Class3 =  @onTypeMismatch("Ignore") class {
-                    @is(String) fieldA;
-                    @is("string") fieldB;
+                    @is(Symbol) fieldA;
+                    @is("symbol") fieldB;
                     // this use-case is broken for the time being
-                    // @is(String) fieldC = "testC";
-                    // @is("string") fieldD = "testD";
-                    @is(String) @onTypeMismatch("Throw") field1;
-                    @is("string") @onTypeMismatch("Throw") field2;
-                    @is(String) @onTypeMismatch(TypeMismatchSetOptions.Throw) field3;
-                    @is("string") @onTypeMismatch(TypeMismatchSetOptions.Throw) field4;
-                    @is(String) @onTypeMismatch("Throw") field5 = "true";
+                    // @is(Symbol) fieldC = "testC";
+                    // @is("symbol") fieldD = "testD";
+                    @is(Symbol) @onTypeMismatch("Throw") field1;
+                    @is("symbol") @onTypeMismatch("Throw") field2;
+                    @is(Symbol) @onTypeMismatch(TypeMismatchSetOptions.Throw) field3;
+                    @is("symbol") @onTypeMismatch(TypeMismatchSetOptions.Throw) field4;
+                    @is(Symbol) @onTypeMismatch("Throw") field5 = symbol1;
                 };
 
                 model3 = new Class3();
@@ -182,7 +189,7 @@ describe("@onTypeMismatch decorator tests throw option for boolean type", () => 
             expect(() => model3.field2 = false).toThrow();
             expect(() => model3.field3 = 1).toThrow();
             expect(() => model3.field4 = 100.1).toThrow();
-            expect(model3.field5).toEqual("true");
+            expect(model3.field5).toEqual(symbol1);
         });
     });
 });
