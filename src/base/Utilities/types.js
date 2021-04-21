2b95c7cfdde4aef5950bf0f1c2af83bffec379a7
Michael A. Vickers <mavickers@lightpathllc.com>
2021-04-21 15:43:12 -0400

    WIP - fixing isType.

--- src/base/Utilities/types.js
+++ src/base/Utilities/types.js
@@ -1,7 +1,8 @@
 import Globals from "../Globals";
 
+const allTypes = [...Globals.Primitives, ...Globals.Structurals ];
+
 const getType = (obj) => {
-    const allTypes = [...Globals.Primitives, ...Globals.Structurals ];
     const objTypeString = Object.prototype.toString.call(obj);
     const types = allTypes.filter(t => t.signature === objTypeString);
 
@@ -9,10 +10,16 @@ const getType = (obj) => {
 };
 
 const isType = (obj, type) => {
+    // this compares on a strict basis - anything that can return true
+    // as typeof Object (such as Array and other structures) will return
+    // false here.
+
     if (obj === null || obj === undefined || type === null || type === undefined) return false;
     if (typeof type == "string") return typeof obj === type;
-    if (obj instanceof type) return true;
-    if (Globals.Primitives.find(p => p.type === type) && typeof obj === type.name.toLowerCase()) return true;
+    if (allTypes.find(t => t.type === type && Object.prototype.toString.call(obj) === t.signature)) return true;
+    console.log(1);
+    //if (Globals.Primitives.find(p => p.type === type) && typeof obj === type.name.toLowerCase()) return true;
+    if (type !== Object && obj instanceof type) return true;
 
     return false;
 }
